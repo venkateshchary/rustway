89bfeb79248c09e2076097efd047c10961e27b91
VENKATESH <vchary78@gmail.com>
2025-12-09 08:27:05 +0530

    try_operaor intro

--- packages_lib_6_1/src/file_reader.rs
+++ packages_lib_6_1/src/file_reader.rs
@@ -1,3 +1,8 @@
+/*
+    I am using a standard library to create a file
+ */
+use std::fs::File;
+use std::io::Write;
 
 #[derive(Debug)]
 pub enum FileTypes{
@@ -19,10 +24,47 @@ pub fn check_file_pattern(file_path: &str) -> Option<FileTypes>{
     }
 }
 
+pub fn find_extension_using_split(file_path: &str) -> Option<String>{
+    println!("Checking file pattern using the string split method");
+    let extension = file_path.rsplit('.').next()?;
+    Some(extension.to_string())
+}
+
+
 pub fn is_file_exist(file_path: &str) -> bool{
     if file_path.is_empty(){
         false
     }else {
         true
     }
+}
+/*
+? must return Err(e) to the caller
+ */
+
+/*
+     If you don't want to use the ? you can explicitly write match flow with ok and error
+     scenario 1:
+        I am trying to pass empty file name as param it is raising an error
+        Failed to create a file error: Os { code: 3, kind: NotFound, message: "The system cannot find the path specified." }
+
+ */
+pub fn create_new_file_1(file_name: &str)-> Result<File, std::io::Error>{
+    let mut file = File::create(file_name);
+    // file.write_all(b"file created using the std library\n")?;
+    match file {
+        Ok(file) => {Ok(file)},
+        Err(e) => {
+            println!("Failed to create file: {:?}", e);
+            Err(e)
+        }
+    }
+    // file.write_all(b"file created using the std library\n")?;
+    // Ok(file)
+}
+
+pub fn create_new_file(file_name: &str)-> Result<File, std::io::Error> {
+    let mut file = File::create(file_name)?;
+    file.write_all(b"file created using the std library\n")?;
+    Ok(file)
 }
--- packages_lib_6_1/src/main.rs
+++ packages_lib_6_1/src/main.rs
@@ -2,7 +2,16 @@ mod file_reader;
 
 fn main() {
     println!("Hello, world!");
-    let input_file_path = "employee.txt";
+    let input_file_path = "";
+    match file_reader::create_new_file(input_file_path){
+        Ok(msg)=>{
+            println!("Successfully created a file :{:?}", msg);
+        }
+        Err(e) =>{
+            println!("Failed to create a file error: {:?}", e);
+        }
+    }
+
     // file_reader::check_file_pattern();
     match file_reader::check_file_pattern(input_file_path) {
         Some(file_type) => {
@@ -11,5 +20,29 @@ fn main() {
         None =>{
             println!("No file pattern found");
         }
+    }
+
+    println!("Verify the file extension using the split method");
+
+    let file_extension = file_reader::find_extension_using_split(input_file_path);
+    println!("File extension: {:?}", file_extension); // File extension: Some("")
+    // here we can use the match statement instead of direct println!
+
+    match file_extension {
+        Some(extension) => {
+            if extension.is_empty(){
+                println!("No file extension found"); // println!("File extension: {:?}", extension);
+            }
+            else {
+                println!("File extension: {:?}", extension);
+            }
+        }
+        None =>{
+            println!("No file extension found");
+        }
+    }
+
+    if file_reader::is_file_exist(input_file_path) {
+        println!("File already exists");
     }
 }
